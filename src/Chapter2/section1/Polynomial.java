3752c5349453daf66c57d84d50f18ac36b49a5eb
Ppuree Song <roo0622@gmail.com>
2021-06-11 23:05:40 +0900

    클래스, 객체, 참조변수 4

---
+++ src/Chapter2/section1/Polynomial.java
@@ -0,0 +1,8 @@
+package Chapter2.section1;
+
+public class Polynomial {
+
+    public char name;       //다항식의 이름
+    public Term[] terms;    //항
+    public int nTerms = 0;  //다항식을 구성하는 항의 개수
+}
---
+++ src/Chapter2/section1/Term.java
@@ -0,0 +1,7 @@
+package Chapter2.section1;
+
+public class Term {
+
+    public int coef; // 계수 coefficient
+    public int expo; // 자수 exponent
+}
